ad5df08fb7c45d2fce5f9d66e0b6889e32a1026f
Mettadore <john@mettadore.com>
2009-09-01 12:51:08 -0700

    Added some flow control routines that successfully mimic fully functional histogram bars. Added some documentation.

--- Rose.py
+++ Rose.py
@@ -2,42 +2,93 @@ from GChartWrapper import *
 from math import pi, radians, degrees, sin, cos, atan2
 
 class Rose(object):
-    def __init__(self, lst, bad_value=-32768, color='red'):
+    def __init__(self, lst, bad_value=-32768, color='red', size=200):
         self.color = color
         lst = [i for i in filter(lambda x: x != bad_value, lst)]
         coding = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
         bins = {0:0} # Initialize with a zero value at due north
-        a = 0
+        self.size = size
+        ####################################################
         # Separate values into bins of 10 degree increments. The values
         # Are the number of azimuth readings within each 10 degree increment
         # where 10 degrees includes all readings in the range (5,15], 20
         # degrees includes all readings in the range (15,25], and 0 degrees
         # includes all readings in the range (355,5].
+        a = 0
         for b in range(5,360,10):
             rng = lambda x: (x >= a) and (x < b)
             bins[b+5] = len([i for i in filter(rng, lst)])
             a = b
-
+        ############################################
+        # Move values at 360 to zero and copy the full set of values
+        # to 360. This is so that we can have a full circle of data
+        # rather than having the data end at 355.
         try:
-            # Move values at 360 to zero and copy. This is so that we
-            # can have a full circle of data
             bins[0] += bins[360]
             bins[360] = bins[0]
         except KeyError:
             pass
 
+        ############################################
+        # Grab the highest value so that we can have a max and
+        # percentages of the max for the histogram bar heights
+        # and set up some convenience variables for use in the
+        # loops
         highest = max(bins.values())
-        # Re-interpret values as percentages of a hex value from 1-16
-        for k,v in bins.items():
-            try:
-                bins[k] = coding[int((v/highest) * len(coding))]
-            # We have no azimuth values.
-            except ZeroDivisionError:
-                bins[k] = 'A'
-        vals = '' # values at each label
-        for i in range(0,370,10):
-            try: vals += bins[i]
-            except KeyError: vals += 'A' # Minimum if there's no value
+        vals = [] # String of values for use in the URL
+        # The outer loop looks only at values at increments
+        # of ten. This is a variable to help build the inner
+        # loop (values 1-9)
+        a = 0 
+        ############################################
+        # Go through all of our values at ten degree increments
+        # if the degrees are 5 (25, 255, etc) then we set the
+        # value equal to 'A' (i.e. zero). Otherwise, set all the
+        # values from 6-9 and 0-4 to whatever percentage we have.
+        # This ends up creating a zero point at 5, values at some
+        # magnitude centered on a ten degree increment, and then
+        # another zero point at 15. The result is something that
+        # visually resembles a bar extending from zero
+        ###
+        # NOTE: We have so many points only because we want a single
+        # zero point at each 5, 15, 25... degree interval in order
+        # to make the bars. Because of that, and because we want a
+        # flat bar top, we have have to have values at 1 degree increments
+        # so we fill it up with data. It makes a long URL, but I'll
+        # accept that until Google gets a more functional API
+        for i in range(5,360,10):
+            num = bins[i+5]
+            key = int((bins[i+5]/highest) * (len(coding)-1))
+            val = coding[key]
+            for j in range(a,i):
+                if (j%10 == 5): 
+                    vals.append('A')
+                else:
+                    vals.append(val)
+            a = i
+        # Copy the first 5 values to the end of the dataset to complete
+        # the circle (otherwise, 355-360 is blank)
+        vals += vals[:5]
+        # There's a slight gap between 359 and 0, so we add whatever
+        # value is at 0 to fully close that circle. This north bar is
+        # the hackiest bar of all, because the radar diagram WANTS to
+        # go to 361 and higher.
+        vals += vals[0]
+
+        ######################################################
+        # Now we generate labels. We need as many labels as we have
+        # datapoints, but most of them we fill with blank strings.
+        # Here, we make N,S,E,W labels as well as labels at every
+        # ten degrees.
+        labels = [0]
+        mod = 0
+        for i in ['N','E','S','W']:
+            labels.append(i)
+            for j in range(1,90):
+                if not j%10: labels.append(j+mod)
+                else: labels.append('')
+            mod += 90
+
 
         if not len(lst): mean = 0 # We're blank, throw up a placeholder chart
         else:
@@ -53,27 +104,30 @@ class Rose(object):
             # Take the arctan of this, using atan2 to preserve the quadrant information
             # We divide this (in degrees) by ten because there are only 36 values
             # around our rose diagram
-            self.mean = mean = degrees(rotate(atan2(sa,ca)))/10
+            self.mean = mean = degrees(rotate(atan2(sa,ca)))
             ########################################
         
         ########################################
         # Create the pseudo-rose diagram using Google's Radar
         self.G = Radar([vals],encoding='simple')
-        self.G.size(200,200)
+        self.G.size(size,size)
         self.G.color(self.color)
-        self.G.line(2,4,0)
+        self.G.line(1,2,0)
         self.G.axes('x')
-        labels = [0, 'N','','','','','','','','','E','','','','','','','','','S','','','','','','','','','W','','','','','','','','']
         self.G.axes.label(*labels)
         self.G.axes.range(0, 0.0,360.0)
         self.G.marker('h','blue',0,1.0,1)
         self.G.marker('V','008000',0,mean,5)
         self.G.marker('B','FF000080',0,1.0,5.0)
 
-
+    @property
     def URL(self):
         return self.G.url
     def tag(self, imgid='chart'):
         return self.G.img(height=200,id=imgid)
 
-#l = [340.16, 315.02, 313.17, 24.42, 316.28, 325.29, 343.25, 315.32, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768, -32768]
+if __name__ == "__main__":
+    # Generate a rose with a sample dataset.
+    l =[111, 266, 169, 232, 128, 208, 196, 95, 230, 148, 182, 193, 161, 194, 147, 139, 201, 155, 177, 145, 152, 173, 163, 143, 196, 166, 183, 198, 215, 198, 172, 199, 208, 173, 188, 188, 140, 163, 150, 150, 144, 178, 168, 212, 195, 153, 178, 152, 213, 156, 196, 134, 122, 228, 218, 162, 219, 147, 170, 258, 332, 295, 332, 350, 315, 18, 26, 48, 22, 21, 0, 0, 1,1,1,1,4,10,10,9,9,9,9,9]
+    r = Rose(l, size=500)
+    print(r.URL)
